866ade7406230a6ef7d563ad733a4e0bf592c8d7
김예린 <iylin@MacBook-Air.FF_GN1_UTM_17-18F_USG310>
2021-06-09 15:12:43 +0900

    Create the HeroService and Refactor HeroesComponent

---
+++ src/app/hero.service.ts
@@ -0,0 +1,16 @@
+import { HEROES } from './mock-heroes';
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HeroService {
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    return heroes;
+  }
+
+  constructor() {}
+}
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,3 +1,4 @@
+import { HeroService } from './../hero.service';
 import { HEROES } from './../mock-heroes';
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
@@ -8,15 +9,20 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
-
+  heroes: Hero[] = [];
   selectedHero?: Hero;
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
-    console.log(this.selectedHero)
   }
 
-  constructor() {}
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
 
-  ngOnInit(): void {}
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 }
