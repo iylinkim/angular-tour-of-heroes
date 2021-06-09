e75886343b0b607f48a173bbea87908e13be2ee2
김예린 <iylin@MacBook-Air.FF_GN1_UTM_17-18F_USG310>
2021-06-09 14:01:50 +0900

    Add detail info and selected class

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,6 +10,12 @@ import { Hero } from '../hero';
 export class HeroesComponent implements OnInit {
   heroes = HEROES;
 
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    console.log(this.selectedHero)
+  }
+
   constructor() {}
 
   ngOnInit(): void {}
